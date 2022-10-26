5944734dd15cb4d8d5d10540c3f2756a24389282
elzp <86740659+elzp@users.noreply.github.com>
2022-10-26 21:00:17 +0200

    Make not draggable everything but Articles

--- DragAndDrop.js
+++ DragAndDrop.js
@@ -9,20 +9,29 @@ function DragAndDrop({ name }) {
     { name: '3Article', date: '10.09.2022', content: '3Article' },
     { name: '4Article', date: '10.09.2022', content: '4Article' },
   ];
+
   return (
     <div className="flex justify-center items-top">
-      <div className="bg-white overflow-hidden rounded-md p-30 mx-5 max-w-xs flex-1 flex flex-col">
-        <h2 className="text-4xl p-5 text-center">Drafts</h2>
-        <main className="p-10 bg-gray-300 flex-1">
+      <div
+        className="bg-white overflow-hidden rounded-md p-30 mx-5 max-w-xs flex-1 flex flex-col"
+        draggable="false">
+        <h2 className="text-4xl p-5 text-center" draggable="false">
+          Drafts
+        </h2>
+        <main className="p-10 bg-gray-300 flex-1" draggable="false">
           {dataForArticles.map((it) => (
-              <Arcicle key={it.name} props={it} />
+            <Arcicle draggable="false" key={it.name} props={it} />
           ))}
         </main>
       </div>
 
-      <div className="bg-white overflow-hidden rounded-md p-30 max-w-xs flex-1 flex flex-col">
-        <h2 className="text-4xl p-5 text-center">Published</h2>
-        <main className="p-10 bg-gray-300 flex-1"></main>
+      <div
+        className="bg-white overflow-hidden rounded-md p-30 max-w-xs flex-1 flex flex-col"
+        draggable="false">
+        <h2 className="text-4xl p-5 text-center" draggable="false">
+          Published
+        </h2>
+        <main className="p-10 bg-gray-300 flex-1" draggable="false"></main>
       </div>
     </div>
   );
